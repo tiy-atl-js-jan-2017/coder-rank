755ce7aa0651ec936cf03dad09052599bfb5b4b2
Brit Butler <redline6561@gmail.com>
2017-02-15 11:07:17 -0500

    Add automatic wait+retry if Github data isn't ready.

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,37 @@
 import $ from 'jquery';
 
 var BASE_URL = "https://api.github.com";
+var retryCount = 0;
 
-function getRepoStats (event) {
-  event.preventDefault();
+function displayStats (data, status, request) {
+  var messages = $(".messages");
 
+  if (request.status === 202) {
+    retryCount++;
+    messages.empty();
+    messages.append(`<p>This is retry number ${retryCount}.</p>`);
+    messages.append("<p>The data for that project is being processed.</p>");
+    messages.append("<p>We will resend your request in 60 seconds. 😍</p>");
+    setTimeout(function () { fetchData(); }, 60000);
+  } else {
+    console.log(data);
+  }
+}
+
+function fetchData () {
   var user = $("#user-name").val();
   var repo = $("#repo-name").val();
 
   $.ajax({
     url: `${BASE_URL}/repos/${user}/${repo}/stats/contributors`,
     dataType: "json",
-    success: function (data) {
-      console.log(data);
-    }
+    success: displayStats
   });
+}
+
+function getRepoStats (event) {
+  event.preventDefault();
+  fetchData();
 }
 
 var form = $("form");
